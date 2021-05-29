d5aa7234557c1304617b668b13033d5ad5d5b503
Liberato Aguilar <liberatoalanaguilar@gmail.com>
2021-05-28 19:46:51 -0600

    Example 1 .cpp

---
+++ examples/example1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <unistd.h>
+#include <string>
+#include "screen.h"
+
+using std::cin, std::cout, std::endl;
+using std::to_string;
+
+constexpr int timeDelay = 2000;
+
+void drawLoad(Screen& s)
+{
+    s.hideCursor();
+    for (int i = 1; i <= s.getHeight(); ++i)
+    {
+        if (i % 2 == 0)
+        {
+            s.setColor(COLORS::FG::RED);
+        } else {
+            s.setColor(COLORS::FG::BLUE);
+        }
+        if (i < 10)
+        {
+            s.print(to_string(i)+".  ");
+            for (int j = 0; j < s.getWidth()-5; ++j)
+            {
+                s.print('@');
+                s.flush();
+                usleep(timeDelay);
+            }
+        } else {
+            s.print(to_string(i)+". ");
+            for (int j = 0; j < s.getWidth()-5; ++j)
+            {
+                s.print('@');
+                s.flush();
+                usleep(timeDelay);
+            }
+        }
+        s.print('\n');
+    }
+}
+
+int main()
+{
+    Screen s;
+    drawLoad(s);
+    return 0;
+}
